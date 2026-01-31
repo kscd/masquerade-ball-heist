6a7891e1c4ec3ba449eed7627c8f127b928d5a1f
Samuel Effler <samuel.effler@gmail.com>
2026-01-31 14:27:01 +0100

    feat: make movement independent from body

--- scripts/movement.gd
+++ scripts/movement.gd
@@ -1,15 +1,16 @@
-extends CharacterBody2D
+extends Node
 
 @export var max_speed = 300.0
 @export var acceleration = 1500.0
 @export var friction = 1200.0
 
+@onready var body: CharacterBody2D = get_parent()
 var input_direction = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
 	if input_direction != Vector2.ZERO:	
-		velocity = velocity.move_toward(input_direction * max_speed, acceleration * delta)
+		body.velocity = body.velocity.move_toward(input_direction * max_speed, acceleration * delta)
 	else:
-		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+		body.velocity = body.velocity.move_toward(Vector2.ZERO, friction * delta)
 	
-	move_and_slide()
+	body.move_and_slide()
