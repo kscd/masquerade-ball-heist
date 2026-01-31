a5116eb91c8cfbdcea4c216acc4bedeaa223ef68
= <=>
2026-01-31 13:11:53 +0100

    feat: add click signal and gamestate

--- scripts/gamesstate.gd
+++ scripts/gamesstate.gd
@@ -3,8 +3,13 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("Test")
+	print("Gamestate loaded 💫")
 
+# func _on_cursor_object_clicked():
+# 	print("Handle click in state")
+
+func _on_cursor_mouse_event(hit):
+	print("HIT 📍 ", hit)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
